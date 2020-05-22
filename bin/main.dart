77aca720dd0ca8061eb2012d4ef42f275d1c3313
mcclayac <anthony.mcclay@gmail.com>
2020-05-21 23:27:53 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,6 @@
+//code 1.19
+main(List<String> arguments) {
+  String bengaliString = "বাংলা লেখা";
+  String englishString = "This is some English text.";
+  print("Here is some Bengali script - ${bengaliString} and some English script ${englishString}");
+}
